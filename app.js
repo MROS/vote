7e217d9660573cad9bccba69071a65ba73c44c12
MROS <yc1043@gmail.com>
2016-02-07 11:39:38 +0800

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+var koa = require('koa');
+var logger = require('koa-logger');
+var serve = require('koa-static');
+var router = require('koa-router')();
+
+var app = koa();
+app.use(logger());
+app.use(serve('public'));
+
+app.use(function *(){
+  this.body = 'jjjj World';
+});
+
+app.use(router.routes())
+   .use(router.allowedMethods());
+
+app.listen(3000);
